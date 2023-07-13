4071ab6b88c1daece497e1e4cba713945aa401f0
noha elnagar <eng.noha17@gmail.com>
2023-07-13 12:29:37 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints output using puts function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
