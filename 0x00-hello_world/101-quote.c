4fe4b649401b166521b92ed1e2883dadfcdc9455
noha elnagar <eng.noha17@gmail.com>
2023-07-13 15:12:07 -0700

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print a quote using write function
+ * write(int fd.cons void *buf,size_t count
+ *
+ * Return: 1 (fail)
+*/
+
+int main(void)
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.quo.59);
+	return (1);
+}
