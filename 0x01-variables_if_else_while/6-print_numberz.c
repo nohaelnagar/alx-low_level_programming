942e38119b55037a85f1031c96529729507c7ab4
noha elnagar <eng.noha17@gmail.com>
2023-07-14 16:19:49 -0700

    use putchar to display numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,18 +7,18 @@
  * of base 10 starting from 0
  *
  * Return: 0 (success)
- */
+*/
 
 int main(void)
 {
 	int digit = 0;
 
-while (digit <= 9)
-{
-putchar(digit + '0');
-digit++;
-}
-putchar("\n");
+	while (digit <= 9)
+	{
+		putchar(digit + '0')
+		digit++;
+	}
+	putchar("\n");
 
-return (0);
+	return (0);
 }
