b4d43c2cacd8918fb4d98537671e74727e7eba9d
noha elnagar <eng.noha17@gmail.com>
2023-07-18 09:18:28 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a newline
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
