3401f1ad3b28ae62f5b5f47f30093f633b0a7643
noha elnagar <eng.noha17@gmail.com>
2023-07-18 11:16:51 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,13 @@ int print_last_digit(int n)
 	int digit;
 
 	if (n < 0)
-	{
 		n = -n;
-		digit = n % 10;
-	}
+
+	digit = n % 10;
+
 	else
 		digit = n % 10;
-	_putchar(digit + '0');
-	return (digit);
+	_putchar(a + '0');
+
+	return (a);
 }
