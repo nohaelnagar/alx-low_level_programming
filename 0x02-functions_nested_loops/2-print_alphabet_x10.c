5c1da161aa866867493f2879a5eb66a2ad8e1e45
noha elnagar <eng.noha17@gmail.com>
2023-07-18 09:47:11 -0700

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int n;
-	
+
 	for (n = 0; n <= 9; n++)
 	{
-		for ( ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
