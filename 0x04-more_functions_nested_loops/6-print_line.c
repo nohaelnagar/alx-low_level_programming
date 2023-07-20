a443576cf02a9c042f8422f4af4cb9bb442a739d
noha elnagar <eng.noha17@gmail.com>
2023-07-20 16:29:24 -0700

    ds

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character should be printed
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
@@ -12,10 +12,12 @@ void print_line(int n)
 	} else
 	{
 		int i;
+
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
+
 }
