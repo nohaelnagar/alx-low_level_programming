23bb51cbf89b4fd583d419279c80f902a29ec9b2
noha elnagar <eng.noha17@gmail.com>
2023-07-20 11:24:58 -0700

    most nom

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
+		if (i != 2 && i != 4)
 		{
-			i++;
-		}
-		else
 			_putchar(i + '0');
+		}
+		i++;
 	}
 	_putchar('\n');
 }
