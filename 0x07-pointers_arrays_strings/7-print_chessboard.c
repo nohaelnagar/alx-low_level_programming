e2c3c3ea5b87f78b01a6ad2cdc9dffc06a52d84e
noha elnagar <eng.noha17@gmail.com>
2023-07-31 10:17:54 -0700

    chss

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * which is two dimension array 8*8
+ */
+void print_chessboard(char (*a)[8])
+{
+	int ver, hor;
+
+	for (ver = 0; ver < 8; ver++)
+	{
+		for (hor = 0; hor < 8; hor++)
+		{
+			_putchar(a[ver][hor]);
+		}
+		_putchar('\n');
+	}
+}
