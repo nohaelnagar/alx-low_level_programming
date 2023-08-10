c36a8a722e2817c58e56f88e91047126f82a692f
noha elnagar <eng.noha17@gmail.com>
2023-08-10 07:02:17 -0700

    bjh

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int x = 0, y = 0, s1_len = 0, s2_len = 0;
 
 	while (s1 && s1[s1_len])
-		len1++;
+		s1_len++;
 	while (s2 && s2[s2_len])
 		s2_len++;
 
