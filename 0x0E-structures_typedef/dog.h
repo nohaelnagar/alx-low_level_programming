4246eda9232e73fc3f92f0a91fdad7e688d71e03
noha elnagar <eng.noha17@gmail.com>
2023-08-14 15:18:16 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
