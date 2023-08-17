087849a30aa9c1e51e54eb5ee61d056d87271354
noha elnagar <eng.noha17@gmail.com>
2023-08-17 14:10:24 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	int i, sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	va_start(args, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum = sum + va_arg(args, int);
+		sum += va_arg(ap, int);
 
-	va_end(args);
+	va_end(ap);
 
 	return (sum);
 }
