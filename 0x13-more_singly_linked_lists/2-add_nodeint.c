e1558e6e74d85b2e9bb6e84da37cb0fd6f8b344a
noha elnagar <eng.noha17@gmail.com>
2023-08-29 01:51:45 -0700

    ff

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,4 +7,20 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *ptr;
 
+	ptr = malloc(sizeof(listint_t);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr->n = n;
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
