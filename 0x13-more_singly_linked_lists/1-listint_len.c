9b672d89daea86769d4fff5ac72570611865f59b
noha elnagar <eng.noha17@gmail.com>
2023-08-29 00:37:31 -0700

    node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * listint_len - the length of linked list
+ * @h: the list
+ * Return: the number of node
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+	*ptr;
+
+	if (h == NULL)
+		return (NULL);
+
+	ptr =  h->next;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		len++;
+	}
+	return (len);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * add_nodeint- add new node to linked list
+ * @head: the head
+ * @n: the neww added element
+ * Return: the new added element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
