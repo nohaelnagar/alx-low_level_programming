f9604f904ac0523dd503a7709056f59165c40520
noha elnagar <eng.noha17@gmail.com>
2023-08-29 06:56:28 -0700

    ad

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,7 +8,7 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	while (*ptr != NULL)
+	while (head != NULL)
 	{
 		ptr = head->next;
 		free(head);
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,7 +16,7 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		ptr = *head->next;
+		ptr = (*head)->next;
 		free(*head);
 		*head = ptr;
 	}
