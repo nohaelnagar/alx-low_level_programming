36dc9fdabd19fa85d7f365fb75a7792b20375273
nohaelnagar <eng.noha17@gmail.com>
2023-11-03 20:28:20 +0100

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,7 +7,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int index;
+	unsigned long int index;
 	hash_node_t *ptr;
 
 	if (ht == NULL || key == NULL || ht->array == NULL ||
