52077e40d0de5e648ac4c139e9365b7ef67bba9e
nohaelnagar <eng.noha17@gmail.com>
2023-11-03 20:39:02 +0100

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *ptr;
 
 	if (ht == NULL || key == NULL || ht->array == NULL || ht->size == NULL
-	|| strlen(key) == NULL)
+	|| strlen(key) == 0)
 		return (NULL);
 
 	indx = key_index((const unsigned char *)key, ht->size);
