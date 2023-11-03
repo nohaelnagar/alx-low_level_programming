3c622431fab526e78d1531aee826663483251cc6
nohaelnagar <eng.noha17@gmail.com>
2023-11-03 20:58:29 +0100

    task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	int num = 0;
 
 	if (ht == NULL || ht->array == NULL)
-		return (NULL);
+		return;
 	printf("{");
 	for (x = 0; x < ht->size; x++)
 	{
