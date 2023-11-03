46dd64c8ae59b409763e34e627ca9c52bf0b2df9
nohaelnagar <eng.noha17@gmail.com>
2023-11-03 18:46:48 +0100

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,12 +6,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long hash;
+	unsigned long int hash;
 	int c;
 
 	hash = 5381;
-	while (c = *str++)
+	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	}
 	return (hash);
 }
